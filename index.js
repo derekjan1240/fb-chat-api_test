9caf78895a7c4e404603cf13f504114ae6abbcd8
Derek Zhan (Pork) <derekjan1240@gmail.com>
2018-08-01 19:37:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const key = require("./config/keys.js")
 login({email: key.botmail.emailID , password: key.botmail.emailSecret}, (err, api) => {
     if(err) return console.error(err);
 
-    var yourID = "";
-    var msg = "Hi!";
+//     var yourID = "";
+//     var msg = "Hi!";
 
     //api.sendMessage(msg, yourID);
     setInterval( function(){ api.sendMessage(key.receiver.ID, key.receiver.Msg); }, 1000);
